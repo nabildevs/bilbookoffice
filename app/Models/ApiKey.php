07a87b71efdc771c4dc4474e7c93a455eb20a1e4
Hasbi Nabil <hasbinabil.work@gmail.com>
2024-10-25 21:00:20 +0700

    feat: update all model guarded id

--- app/Models/ApiKey.php
+++ app/Models/ApiKey.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ApiKey extends Model
 {
-    //
+    protected $guarded = [
+        'id'
+    ];
 }
--- app/Models/OfficeSpace.php
+++ app/Models/OfficeSpace.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class OfficeSpace extends Model
 {
-    //
+    protected $guarded = [
+        'id'
+    ];
 }
