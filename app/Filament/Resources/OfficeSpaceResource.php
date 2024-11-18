2e18ce749a55f2c73fa40b950c9da461981fad2a
Hasbi Nabil <hasbinabil.work@gmail.com>
2024-11-18 14:42:43 +0700

    feat: finish CRUD OfficeSpace

--- app/Filament/Resources/OfficeSpaceResource.php
+++ app/Filament/Resources/OfficeSpaceResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +24,76 @@ class OfficeSpaceResource extends Resource
     {
         return $form
             ->schema([
-                //
+                // Form Inputs
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->image()
+                    ->required(),
+
+                Forms\Components\RichEditor::make('about')
+                    ->required()
+                    ->toolbarButtons([
+                        'bold',
+                        'italic',
+                        'underline',
+                        'strike',
+                        'link',
+                        'orderedList',
+                        'unorderedList',
+                        'blockquote',
+                        'codeBlock',
+                    ]),
+
+                Forms\Components\Repeater::make('photos')
+                    ->relationship('photos')
+                    ->schema([
+                        Forms\Components\FileUpload::make('photo')
+                            ->required()
+                    ]),
+
+                Forms\Components\Repeater::make('benefits')
+                    ->relationship('benefits')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                    ]),
+
+                Forms\Components\Select::make('city_id')
+                    ->relationship('city', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
+
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
+
+                Forms\Components\Select::make('is_open')
+                    ->options([
+                        true  => 'Open',
+                        false => 'Not Open'
+                    ])
+                    ->required(),
+
+                Forms\Components\Select::make('is_full_booked')
+                    ->options([
+                        true  => 'Not Available',
+                        false => 'Available'
+                    ])
+                    ->required(),
             ]);
     }
 
@@ -31,10 +101,30 @@ class OfficeSpaceResource extends Resource
     {
         return $table
             ->columns([
-                //
+                // Fetch cities data
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\ImageColumn::make('thumbnail')
+                    ->alignCenter(),
+
+                Tables\Columns\TextColumn::make('city.name')
+                    ->alignCenter(),
+
+                Tables\Columns\IconColumn::make('is_full_booked')
+                    ->boolean()
+                    ->trueColor('danger')
+                    ->falseColor('success')
+                    ->trueIcon('heroicon-o-x-circle')
+                    ->falseIcon('heroicon-o-check-circle')
+                    ->label('Available')
+                    ->alignCenter(),
             ])
             ->filters([
-                //
+                // Filter datas
+                SelectFilter::make('city_id')
+                    ->relationship('city', 'name')
+                    ->label('City'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
