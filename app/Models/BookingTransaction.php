3b4440616439b7224f82d29edfaaf2598a743be4
Hasbi Nabil <hasbinabil.work@gmail.com>
2024-11-19 14:26:03 +0700

    feat: add generateUniqueTrxId() BookingTransaction model

--- app/Models/BookingTransaction.php
+++ app/Models/BookingTransaction.php
@@ -2,11 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookingTransaction extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $guarded = [
         'id'
     ];
+
+    // Generate unique transaction id
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'TR';
+
+        do {
+            $randString = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randString)->exists());
+
+        return $randString;
+    }
+
+    // Relations
+    public function officeSpace(): BelongsTo
+    {
+        return $this->belongsTo(OfficeSpace::class, 'office_space_id');
+    }
 }
