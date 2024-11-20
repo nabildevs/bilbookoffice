955b8a4340d98fba851d445c474b6b0b64ee44a0
Hasbi Nabil <hasbinabil.work@gmail.com>
2024-11-20 20:47:50 +0700

    feat: add api resources

--- app/Filament/Resources/ApiKeyResource.php
+++ app/Filament/Resources/ApiKeyResource.php
@@ -19,7 +19,7 @@ class ApiKeyResource extends Resource
 
     protected static ?string $navigationGroup = 'Data Management';
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-key';
 
     public static function form(Form $form): Form
     {
