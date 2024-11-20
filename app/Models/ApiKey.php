9e8f1deb4825dc33f5fb97aadfb3359721be1db1
Hasbi Nabil <hasbinabil.work@gmail.com>
2024-11-20 20:30:52 +0700

    feat: add CRUD api key, generate api key auto

--- app/Models/ApiKey.php
+++ app/Models/ApiKey.php
@@ -2,11 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ApiKey extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $guarded = [
         'id'
     ];
+
+    // Generate api key
+    public static function generateApiKey()
+    {
+        $prefix = 'API';
+
+        do {
+            $randString = $prefix . mt_rand(1000000, 999999);
+        } while (self::where('key', $randString)->exists());
+
+        return $randString;
+    }
 }
